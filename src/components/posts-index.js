766925fea4626fd07ee8057ed7dec2040e4d6c4f
coucou_miya <ybb.emily@gmail.com>
2018-05-21 15:23:31 +0200

    fetch posts

---
+++ src/components/posts-index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions' 
+import _ from 'lodash';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  renderPosts(){
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id} >
+          {post.title}
+        </li>
+      );
+    });
+  }
+  render() {
+    console.log(this.props.posts);
+    return (
+      <div>
+        <h3>Posts index</h3>
+        <ul className="list-group">
+          {this.renderPosts()} 
+        </ul>
+      </div>
+    );
+  }
+}
+/*const mapDispatchToProps= (d)=>{
+  // return {fetchPosts : (params)=>d(fetchPosts(params))}
+  return bindActionCreators({ fetchPosts } ,d);
+}*/
+// connect accept the second param as object ( each functions insides assumed to be action creators ) and 
+// function like mapDispatchToProps
+export default connect((state)=>{return { posts: state.posts }}, { fetchPosts })(PostsIndex);
